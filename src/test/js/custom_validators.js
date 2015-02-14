c6e2f06df32edbe34ceef19d52c4daa4dfa4dda7
Luan Nico <luanpotter28@hotmail.com>
2015-02-13 23:30:07 -0200

    Fixing === comparsion of fixed types

--- src/test/js/custom_validators.js
+++ src/test/js/custom_validators.js
@@ -1,7 +1,7 @@
 (function() {
 	var PACKAGE = '@xyz.luan.validum.customs.';
 	validum.validators[PACKAGE + 'ValidAddress'] = function(address) {
-		var invalid = address !== null && (address.number == 404 || address.street == "Bad Street");
+		var invalid = address !== null && (address.number === 404 || address.street === "Bad Street");
 		return invalid ? [ 'ValidAddress.invalidAddress' ] : [];
 	};
 }());
